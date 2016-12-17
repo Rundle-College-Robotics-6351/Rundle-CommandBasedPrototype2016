5d7dda00352760cd9945fed70ba6d0fd06582612
Davis Carlson <Davis Carlson@DAVIS-VR-PC.cg.shawcable.net>
2016-12-16 18:41:16 -0700

    Added basic Autonomous and Teleoperated mode control
    -Added SmartDashboard chooser for autoMode
    
    -Added Forward, Spin, and Come Back for default autonomous mode
    -Added Tank Drive for default teleop mode
    -Added SmartDashboard fields for left and right Joystick axis

--- Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/Robot.java
+++ Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/Robot.java
@@ -5,7 +5,10 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team6351.robot.commands.ExampleCommand;
+
+import org.usfirst.frc.team6351.robot.commands.AutoFwdSpinComeBack;
+import org.usfirst.frc.team6351.robot.commands.TankDrive;
+import org.usfirst.frc.team6351.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team6351.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,11 +22,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final DriveTrain driveTrain = new DriveTrain();
 	public static OI oi;
 
-    Command autonomousCommand;
-    SendableChooser chooser;
+    Command autonomousStart;
+    Command teleopStart;
+    SendableChooser autoMode;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -31,10 +35,9 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
-//        chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
+		autoMode = new SendableChooser();
+		autoMode.addDefault("ForwardSpinComeBack", new AutoFwdSpinComeBack());
+        SmartDashboard.putData("Auto mode", autoMode);
     }
 	
 	/**
@@ -60,7 +63,7 @@ public class Robot extends IterativeRobot {
 	 * or additional comparisons to the switch structure below with additional strings & commands.
 	 */
     public void autonomousInit() {
-        autonomousCommand = (Command) chooser.getSelected();
+    	autonomousStart = (Command) autoMode.getSelected();
         
 		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
 		switch(autoSelected) {
@@ -74,7 +77,7 @@ public class Robot extends IterativeRobot {
 		} */
     	
     	// schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
+        if (autonomousStart != null) autonomousStart.start();
     }
 
     /**
@@ -89,7 +92,10 @@ public class Robot extends IterativeRobot {
         // teleop starts running. If you want the autonomous to 
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
+        if (autonomousStart != null) autonomousStart.cancel();
+        
+        teleopStart = new TankDrive();
+        teleopStart.start();
     }
 
     /**
@@ -97,6 +103,8 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
+        SmartDashboard.putNumber("Left Joystick Y", Robot.oi.driver1.getRawAxis(1));
+        SmartDashboard.putNumber("Right Joystick Y", Robot.oi.driver1.getRawAxis(3));
     }
     
     /**
