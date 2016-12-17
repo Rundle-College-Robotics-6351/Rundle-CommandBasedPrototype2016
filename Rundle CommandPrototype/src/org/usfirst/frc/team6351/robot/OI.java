9beff2936848a64eed8997a33a4b4e0b8908e38f
Davis Carlson <Davis Carlson@DAVIS-VR-PC.cg.shawcable.net>
2016-12-16 18:46:08 -0700

    Removed import of exampleCommand

--- Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/OI.java
+++ Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/OI.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
-import org.usfirst.frc.team6351.robot.commands.ExampleCommand;
 import org.usfirst.frc.team6351.robot.commands.ExtendPiston;
 import org.usfirst.frc.team6351.robot.commands.RetractPiston;
 
