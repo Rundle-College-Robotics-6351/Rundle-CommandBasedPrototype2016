553300706b300638d3097909c083582205832ae0
Davis Carlson <Davis Carlson@DAVIS-VR-PC.cg.shawcable.net>
2016-12-16 20:29:50 -0700

    Added Pneumatics Subsystem
    -Added compressor and solenoid variables
    -Added functions to start compressor
    -Added functions to receive faults from compressor

---
+++ Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/subsystems/Pneumatics.java
@@ -0,0 +1,50 @@
+
+package org.usfirst.frc.team6351.robot.subsystems;
+
+import org.usfirst.frc.team6351.robot.Robot;
+import org.usfirst.frc.team6351.robot.OI;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Pneumatics extends Subsystem {
+	Compressor compressor;
+	Solenoid s1;
+	Solenoid s2;
+	
+	public Pneumatics(){
+		compressor = new Compressor();
+
+	}
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void start() {
+		if (Robot.isReal()) {
+			compressor.start();
+			
+		}
+	}
+    public boolean getEnabled() {
+    	return compressor.enabled();
+    }
+    public boolean getCurrentFault() {
+    	return compressor.getCompressorCurrentTooHighFault();
+    }
+    public boolean getConnectionFault() {
+    	return compressor.getCompressorNotConnectedFault();
+    }
+    public boolean getShortFault() {
+    	return compressor.getCompressorShortedFault();
+    }
+
+}
+
