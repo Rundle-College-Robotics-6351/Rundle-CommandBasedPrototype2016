71bc4a10d807041ca5fe43d4df81c4e0118b63f3
Davis Carlson <Davis Carlson@DAVIS-VR-PC.cg.shawcable.net>
2016-12-16 18:44:44 -0700

    Added Scaling values
    -Added Auto mode scaling to 0.4
    -Added teleop mode scaling to 0.4

--- Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/RobotMap.java
+++ Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/RobotMap.java
@@ -15,4 +15,6 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	public static final double Tank_Drive_Scaling_Teleop = 0.4;
+	public static final double Tank_Drive_Scaling_Auto = 0.4;
 }
