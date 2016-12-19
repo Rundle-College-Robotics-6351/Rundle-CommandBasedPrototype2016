4cba322e114841d1070e2757cb0ba769e3f37413
Rundle-Robotics-6351 <srhigh@rundle.ab.ca>
2016-12-19 13:06:24 -0700

    Added Solenoid funtions
    -Added Solenoid declarations
    -Added X and Y Buttons to second solenoid

--- Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/OI.java
+++ Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/OI.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team6351.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
@@ -12,15 +13,25 @@ import org.usfirst.frc.team6351.robot.commands.RetractPiston;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	public Joystick driver1 = new Joystick(1);
+	public Joystick driver1 = new Joystick(0);
 	
 	public Button driverA = new JoystickButton(driver1, 2);
 	public Button driverB = new JoystickButton(driver1, 3);
+	public Button driverX = new JoystickButton(driver1, 1);
+	public Button driverY = new JoystickButton(driver1, 4);
+	
+	Solenoid s1 = new Solenoid(4);
+	Solenoid s2 = new Solenoid(3);
 	
 	public OI() {
 
-		driverA.whenPressed(new ExtendPiston());
-		driverB.whenPressed(new RetractPiston());
+		//driverA.whenPressed(new ExtendPiston());
+		//driverB.whenPressed(new RetractPiston());
+		driverA.whenPressed(new ExtendPiston(s1));
+		driverB.whenPressed(new RetractPiston(s1));
+		driverX.whenPressed(new ExtendPiston(s2));
+		driverY.whenPressed(new RetractPiston(s2));
+		
 	}
 	//Method for getting an axis value on the driver joystick
 		public double driverAxisValue (int axis) {
