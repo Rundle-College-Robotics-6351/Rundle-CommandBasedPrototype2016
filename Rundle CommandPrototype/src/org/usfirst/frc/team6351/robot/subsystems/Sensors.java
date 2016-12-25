d1388a5e2d27142346598710729a3dee8c5a2e2c
Davis Carlson <Davis Carlson@DAVIS-VR-PC.cg.shawcable.net>
2016-12-25 14:06:10 -0700

    Created Sensor Subsystem
    -Initialized ADXRS450 Gyroscope
    -Added function to get gyroscope angle

---
+++ Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/subsystems/Sensors.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team6351.robot.subsystems;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
+/**
+ *
+ */
+public class Sensors extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+	public Gyro gyro;
+	public Accelerometer accel;
+	
+	public Sensors() {
+		gyro = new ADXRS450_Gyro();
+		accel = new BuiltInAccelerometer();
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public double getGyroAngle() {
+    	return gyro.getAngle();
+    }
+}
+
