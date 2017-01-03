89940580bf3f6af25987662dce8d447099e771ab
Davis Carlson <bigdavisc@shaw.ca>
2017-01-02 17:25:12 -0700

    Added code to make AutoDriveStraight command function

--- Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/commands/AutoDriveStraight.java
+++ Rundle CommandPrototype/src/org/usfirst/frc/team6351/robot/commands/AutoDriveStraight.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team6351.robot.commands;
 
+import org.usfirst.frc.team6351.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,13 +10,22 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class AutoDriveStraight extends Command {
 
-    public AutoDriveStraight() {
+	double spd, tme;
+	
+    public AutoDriveStraight(double speed, double time) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    	spd = speed;
+    	tme = time;
+    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.driveTrain.setLeft(spd*-1);
+    	Robot.driveTrain.setRight(spd);
+    	Timer.delay(tme);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -22,11 +34,13 @@ public class AutoDriveStraight extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrain.setLeft(0);
+    	Robot.driveTrain.setRight(0);
     }
 
     // Called when another command which requires one or more of the same
